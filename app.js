4e7aee6226c5ea09b38576815fac05575430e78e
Waldir Ozuna <wozuna@hotmail.com>
2024-11-18 08:02:57 -0600

    Commit inicuial: Funcion saludos

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Bievenido al programa de saludos.");
+
+function saludar(nombre) {
+    return `Hola, ${nombre}! ¿Como estas?`;
+}
+
+const nombreUsuario = "Estudiante";
+console.log(saludar(nombreUsuario));
+
